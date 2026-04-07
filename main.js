1523e219c3d0b935ae428c33e429947a7d4f10d4
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-04-07 20:13:45 +0300

    Step 12: conditional statements in JavaScript

--- main.js
+++ main.js
@@ -185,6 +185,100 @@
 // let arr2 = [1, 2, 3];
 // console.log(arr1 === arr2); // false 
 
-let a2 = 5;
-let b2 = 2;
-console.log(a2 + b2, a2 * b2);
+// let a2 = 5;
+// let b2 = 2;
+// console.log(a2 + b2, a2 * b2);
+
+// let yourAge = 18;
+
+// if (yourAge >= 18) {
+//     console.log("Доступ разрешён");
+// } else {
+//     console.log("Доступ запрещён");
+// }
+
+// let temperature = 10;
+
+// if (temperature < 0) {
+//     console.log("Холодно");
+// } else if (temperature >= 0 && temperature <= 20) {
+//     console.log("Прохладно");
+// } else {
+//     console.log("Тепло");
+// }
+
+// if (isStudent && age < 25) {
+//     console.log("Доступна студенческая скидка");
+// }
+
+// let isLoggedIn = true;
+// let isAdmin = true;
+
+// if (isLoggedIn && isAdmin) {
+//     console.log("Полный доступ");
+// } else if (isLoggedIn && !isAdmin) {
+//     console.log("Ограниченный доступ");
+// } else {
+//     console.log("Доступ запрещен");
+// }
+
+// let a3 = 10;
+// let b3 = "10";
+
+// console.log("С помощью ==: ", a3 == b3); // num
+// console.log("С помощью ===: ", a3 === b3); // str
+
+// let message = age >= 18 ? "Совершеннолетний" : "Несовершеннолетний";
+// console.log(message)
+
+// let day = 3;
+
+// switch (day) {
+//     case 1: console.log("Пн"); break;
+//     case 2: console.log("Вт"); break;
+//     case 3: console.log("Ср"); break;
+//     default: console.log("Неизвестный день");
+// }
+
+let monthNumber = 6;
+
+switch (monthNumber) {
+    case 1:
+        console.log("Январь");
+        break;
+    case 2:
+        console.log("Февраль");
+        break;
+    case 3:
+        console.log("Март");
+        break;
+    case 4:
+        console.log("Апрель");
+        break;
+    case 5:
+        console.log("Май");
+        break;
+    case 6:
+        console.log("Июнь");
+        break;
+    case 7:
+        console.log("Июль");
+        break;
+    case 8:
+        console.log("Август");
+        break;
+    case 9:
+        console.log("Сентябрь");
+        break;
+    case 10:
+        console.log("Октябрь");
+        break;
+    case 11:
+        console.log("Ноябрь");
+        break;
+    case 12:
+        console.log("Декабрь");
+        break; 
+    default: 
+        console.log("Ошибка: ввидте число от 1 до 12");
+}
